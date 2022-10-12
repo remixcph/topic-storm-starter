0a0219b4cba9526cd94ea994bf273b9a2c4b7e7b
Adi Cucolaș <cucolas.adi@gmail.com>
2022-10-12 17:21:25 +0200

    ✨ feat(like): add unlike logic

--- app/models/like.server.ts
+++ app/models/like.server.ts
@@ -1,26 +1,26 @@
-import type { Topic, User } from '@prisma/client';
+import type { Topic, Like } from "@prisma/client";
 
 import { prisma } from "~/db.server";
 
+export function createLike({ id, userId }: Pick<Topic, "id" | "userId">) {
+  return prisma.like.create({
+    data: {
+      topic: {
+        connect: {
+          id,
+        },
+      },
+      user: {
+        connect: {
+          id: userId,
+        },
+      },
+    },
+  });
+}
 
-export function createLike({
-    id,
-    userId
-}: Pick<Topic, "id"> & {
-  userId: User["id"];
-}) {
-    return prisma.like.create({
-        data: {
-            topic: {
-                connect: {
-                    id,
-                },
-            },
-            user: {
-                connect: {
-                    id: userId,
-                },
-            },
-        }
-    })
+export function deleteLike({ id }: Pick<Like, "id">) {
+  return prisma.like.delete({
+    where: { id },
+  });
 }
